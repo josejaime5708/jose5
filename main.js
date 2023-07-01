c69af74a9fb417a9afc7265bb89f185bf47ac74e
josejaime5708 <138315887+josejaime5708@users.noreply.github.com>
2023-07-01 13:02:49 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("hello !!");
+document.querySelector(".menu-btn").addEventListener("click",()=>{
+    document.querySelector(".nav-menu").classList.toggle("show")
+})
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.newscards',{delay:500});
+ScrollReveal().reveal('.cards-banner-one',{delay:500});
+ScrollReveal().reveal('.cards-banner-two',{delay:500});
